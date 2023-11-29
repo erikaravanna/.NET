ab8a2f54514f1ba6135b1301ed0a9393a689bae5
erikaravanna <erikaravanna@hotmail.com>
2023-11-29 12:11:53 -0300

    quinto commit

--- Program.cs
+++ Program.cs
@@ -1,184 +1,84 @@
-﻿public class Pessoa
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program
 {
-    protected string nome;
-    protected DateTime dataNascimento;
-    protected string cpf;
-
-    public Pessoa(string nome, DateTime dataNascimento, string cpf)
+    static void Main()
     {
-        this.nome = nome;
-        this.dataNascimento = dataNascimento;
-        this.cpf = cpf;
-    }
+        // Exemplo de utilização das classes
+        Medico medico = new Medico("Dr. Silva", new DateTime(1980, 5, 15), "12345678901", "CRM12345");
+        Paciente paciente = new Paciente("Maria", new DateTime(1990, 8, 22), "98765432109", Sexo.Feminino, "Dor de cabeça");
 
-    public string Nome
-    {
-        get { return nome; }
-        set { nome = value; }
-    }
+        // Adicionando pacientes ao médico
+        medico.AdicionarPaciente(paciente);
 
-    public DateTime DataNascimento
-    {
-        get { return dataNascimento; }
-        set { dataNascimento = value; }
-    }
+        // Exibindo relatório de pacientes do médico
+        medico.GerarRelatorioPacientes();
 
-    public string Cpf
-    {
-        get { return cpf; }
-        set { cpf = value; }
+        // Outros exemplos de operações...
     }
 }
 
+// Enum para representar o sexo do paciente
+public enum Sexo
+{
+    Masculino,
+    Feminino
+}
+
+// Classe base para pessoa
+public abstract class Pessoa
+{
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string CPF { get; set; }
+
+    public Pessoa(string nome, DateTime dataNascimento, string cpf)
+    {
+        Nome = nome;
+        DataNascimento = dataNascimento;
+        CPF = cpf;
+    }
+}
+
+// Classe Médico herda de Pessoa
 public class Medico : Pessoa
 {
-    protected string crm;
+    public string CRM { get; set; }
+    private List<Paciente> pacientes;
 
     public Medico(string nome, DateTime dataNascimento, string cpf, string crm)
         : base(nome, dataNascimento, cpf)
     {
-        this.crm = crm;
-    }
-
-    public string Crm
-    {
-        get { return crm; }
-        set { crm = value; }
-    }
-}
-
-public class Paciente : Pessoa
-{
-    protected string sexo;
-    protected string sintomas;
-
-    public Paciente(string nome, DateTime dataNascimento, string cpf, string sexo, string sintomas)
-        : base(nome, dataNascimento, cpf)
-    {
-        this.sexo = sexo;
-        this.sintomas = sintomas;
-    }
-
-    public string Sexo
-    {
-        get { return sexo; }
-        set { sexo = value; }
-    }
-
-    public string Sintomas
-    {
-        get { return sintomas; }
-        set { sintomas = value; }
-    }
-}
-
-// Requisitos
-
-public class ConsultorioMedico
-{
-    private List<Medico> medicos = new List<Medico>();
-    private List<Paciente> pacientes = new List<Paciente>();
-
-    public ConsultorioMedico()
-    {
-    }
-
-    public void AdicionarMedico(Medico medico)
-    {
-        if (!medicos.Any(m => m.Cpf == medico.Cpf))
-        {
-            medicos.Add(medico);
-        }
+        CRM = crm;
+        pacientes = new List<Paciente>();
     }
 
     public void AdicionarPaciente(Paciente paciente)
     {
-        if (!pacientes.Any(p => p.Cpf == paciente.Cpf))
+        pacientes.Add(paciente);
+    }
+
+    public void GerarRelatorioPacientes()
+    {
+        Console.WriteLine($"Relatório de Pacientes do Dr. {Nome} (CRM: {CRM}):");
+        foreach (var paciente in pacientes)
         {
-            pacientes.Add(paciente);
+            Console.WriteLine($"- Nome: {paciente.Nome}, CPF: {paciente.CPF}");
         }
     }
 }
-public class Consultas
+
+// Classe Paciente herda de Pessoa
+public class Paciente : Pessoa
 {
-    public static IEnumerable<Medico> FiltrarMedicosPorIdade(int idadeMinima, int idadeMaxima)
+    public Sexo Sexo { get; set; }
+    public string Sintomas { get; set; }
+
+    public Paciente(string nome, DateTime dataNascimento, string cpf, Sexo sexo, string sintomas)
+        : base(nome, dataNascimento, cpf)
     {
-        return from medico in medicos
-                where medico.DataNascimento.Year - medico.DataNascimento.Month * 12 - medico.DataNascimento.Day >= idadeMinima
-                && medico.DataNascimento.Year - medico.DataNascimento.Month * 12 - medico.DataNascimento.Day <= idadeMaxima
-                select medico;
+        Sexo = sexo;
+        Sintomas = sintomas;
     }
-
-    public static IEnumerable<Paciente> FiltrarPacientesPorIdade(int idadeMinima, int idadeMaxima)
-    {
-        return from paciente in pacientes
-                where paciente.DataNascimento.Year - paciente.DataNascimento.Month * 12 - paciente.DataNascimento.Day >= idadeMinima
-                && paciente.DataNascimento.Year - paciente.DataNascimento.Month * 12 - paciente.DataNascimento.Day <= idadeMaxima
-                select paciente;
-    }
-
-    public static IEnumerable<Paciente> FiltrarPacientesPorSexo(string sexo)
-    {
-        return from paciente in pacientes
-                where paciente.Sexo == sexo
-                select paciente;
-    }
-
-    public static IEnumerable<Paciente> OrdenarPacientesPorNome()
-    {
-        return from paciente in pacientes
-        orderby paciente.Nome
-        select paciente;
-    }
-
-    public static IEnumerable<Paciente> FiltrarPacientesPorSintomas(string sintomas)
-    {
-        return from paciente in pacientes
-                where paciente.Sintomas.Contains(sintomas)
-                select paciente;
-    }
-
-    public static IEnumerable<Pessoa> FiltrarAniversariantes(int mes)
-    {
-        return from pessoa in medicos.Concat(pacientes)
-                where pessoa.DataNascimento.Month == mes
-                select pessoa;
-    }
-}
-
-var medicos = Consultas.FiltrarMedicosPorIdade(25, 35);
-
-foreach (var medico in medicos)
-{
-    Console.WriteLine(medico.Nome);
-}
-
-var pacientes = Consultas.FiltrarPacientesPorIdade(18, 25);
-
-foreach (var paciente in pacientes)
-{
-    Console.WriteLine(paciente.Nome);
-}
-
-var pacientes = Consultas.FiltrarPacientesPorSexo(Console.ReadLine());
-
-foreach (var paciente in pacientes)
-{
-    Console.WriteLine(paciente.Nome);
-}
-
-var pacientes = Consultas.OrdenarPacientesPorNome();
-
-foreach (var paciente in pacientes)
-{
-    Console.WriteLine(paciente.Nome);
-}
-
-var sintomas = Console.ReadLine();
-
-var pacientes = Consultas.FiltrarPacientesPorSintomas(sintomas);
-
-foreach (var paciente in pacientes)
-{
-    Console.WriteLine(paciente.Nome);
 }
